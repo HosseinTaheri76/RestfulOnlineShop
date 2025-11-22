6a23ca413ad62e510289821770aa64faea2c3c74
Hossein Taheri <hosseintaheri76@gmail.com>
2025-11-22 10:01:46 +0330

    Add carts app

---
+++ carts/models.py
@@ -0,0 +1,61 @@
+from uuid import uuid4
+
+from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+from products.models import Product, ProductVariant
+
+class Cart(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid4,
+        editable=False,
+        verbose_name=_('ID'),
+    )
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name=_('User'),
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at'),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at'),
+    )
+
+    def __str__(self):
+        return str(self.id)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        to=Cart,
+        on_delete=models.CASCADE,
+        related_name='items',
+        verbose_name=_('Cart'),
+    )
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.PROTECT,
+        related_name='cart_items',
+        verbose_name=_('Product'),
+    )
+    product_variant = models.ForeignKey(
+        to=ProductVariant,
+        on_delete=models.PROTECT,
+        related_name='cart_items',
+        null=True,
+        blank=True,
+        verbose_name=_('Product Variant'),
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name=_('Quantity'),
+    )
+
